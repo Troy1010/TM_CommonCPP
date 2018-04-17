aded1da031fc8a61ae297f30725792082875a754
Troy1010 <drmcmurderface2@mail.com>
2018-04-17 04:12:54 -0500

    Restructured. Fixed reference properties to allow builds.

--- TM_CommonCPP_Tests/test.cpp
+++ TM_CommonCPP_Tests/test.cpp
@@ -1,6 +1,12 @@
 #include "pch.h"
 
+#include "../TM_CommonCPP/TM_CommonCPP_Main.h"
+
+//Acceptance test
 TEST(TestCaseName, TestName) {
+
+
+	/*
   EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+  EXPECT_TRUE(true);*/
 }
