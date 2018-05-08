86cf5e3356b74bcf4e0ba94a56fa165c95ff9618
Troy1010 <drmcmurderface2@mail.com>
2018-05-07 20:08:00 -0500

    added test project

--- TM_CommonCPP_Tests/test.cpp
+++ TM_CommonCPP_Tests/test.cpp
@@ -1,11 +1,12 @@
 #include "pch.h"
 
-#include "../TM_CommonCPP/TM_CommonCPP_Main.h"
+//#include "../TM_CommonCPP/TM_CommonCPP_Main.h"
+#include <plog/Log.h>
 
 //Acceptance test
 TEST(TestCaseName, TestName) {
 
-
+	LOGD;
 	/*
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);*/
