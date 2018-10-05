b1bc180d4c5a46f170f7d5cd7627328e49f28de0
Unknown <drmcmurderface2@mail.com>
2018-10-05 03:02:52 -0500

    cleanup

---
+++ Script_ConanCreatePackage.py
@@ -0,0 +1,13 @@
+##region Settings
+bPause = True
+##endregion
+import os, sys
+import TM_CommonPy as TM
+
+try:
+    TM.Run("conan create . Troy1010/channel -pr conanprofile_OBSEPlugin")
+except Exception as e:
+    TM.DisplayException(e)
+    sys.exit(1)
+if bPause:
+    TM.DisplayDone()
