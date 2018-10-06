d4efded2143f6459c535ca55f2650f1f727e5054
Unknown <drmcmurderface2@mail.com>
2018-10-05 19:05:29 -0500

    PlogFormatter

---
+++ TM_CommonCPP/PlogFormatter.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "plog/Log.h"
+
+namespace plog
+{
+	class MyFormatter
+	{
+	public:
+		static util::nstring header() // This method returns a header for a new file. In our case it is empty.
+		{
+			return util::nstring();
+		}
+
+		static util::nstring format(const Record& record) // This method returns a string from a record.
+		{
+			util::nostringstream ss;
+			ss << record.getMessage() << "\n"; // Produce a simple string with a log message.
+
+			return ss.str();
+		}
+	};
+}
