ad45421a64c91aba95056372a307dd0d267a629e
Unknown <drmcmurderface2@mail.com>
2018-10-06 00:18:55 -0500

    cleanup

--- conanfile.py
+++ conanfile.py
@@ -12,11 +12,9 @@ class TM_CommonCPP_Conan(ConanFile):
 
     def source(self):
         self.run("git clone -b beta https://github.com/Troy1010/TM_CommonCPP.git")
-        #self.run("git clone https://github.com/SergiusTheBest/plog.git")
 
     def package(self):
         self.copy("*.h", dst="include", src="TM_CommonCPP/TM_CommonCPP/include")
-        #self.copy("*.h", dst="include", src="plog/include")
 
     def package_info(self):
         self.cpp_info.includedirs = ['include']
