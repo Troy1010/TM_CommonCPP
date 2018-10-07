c5c7146f7ba96f7a15fec82e56a85987a8727b82
Unknown <drmcmurderface2@mail.com>
2018-10-06 21:22:51 -0500

    conanfile, RecommendedIntegration

--- conanfile.py
+++ conanfile.py
@@ -9,12 +9,15 @@ class TM_CommonCPP_Conan(ConanFile):
     description = "TMinus1010's common C++ library."
     requires="Plog/0.1@Troy1010/channel"
     generators="visual_studio"
+    exports = "RecommendedIntegration.py"
 
     def source(self):
         self.run("git clone -b beta https://github.com/Troy1010/TM_CommonCPP.git")
 
     def package(self):
+        self.copy("RecommendedIntegration.py")
         self.copy("*.h", dst="include", src="TM_CommonCPP/TM_CommonCPP/include")
+        self.copy("*", dst="TM_CommonCPP", src="TM_CommonCPP")
 
     def package_info(self):
         self.cpp_info.includedirs = ['include']
