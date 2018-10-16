8421510536cee779d721ae38e60df0d3541bb8c2
Unknown <drmcmurderface2@mail.com>
2018-10-16 16:27:43 -0500

    Removed IterableEnum macros, added COUNT_ARGS

--- TM_CommonCPP/include/TM_CommonCPP/Misc.h
+++ TM_CommonCPP/include/TM_CommonCPP/Misc.h
@@ -2,13 +2,6 @@
 
 #include <string>
 
-#ifdef TMCOMMONCPP_EXPORTS
-#define TMCommonCPP_API __declspec(dllexport)
-#else
-//#define TMCommonCPP_API __declspec(dllimport)
-#define TMCommonCPP_API
-#endif
-
 std::string to_string_hex(int iInt);
 std::string string_repeat(int n, std::string s);
 std::string Hello();
