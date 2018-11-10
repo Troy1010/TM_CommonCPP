31ed861138da182e4432cbf0b84f2eb133ce985e
Unknown <drmcmurderface2@mail.com>
2018-11-10 12:53:51 -0600

    Tests-Switched to other Runtime, working but could rewind and do better

--- TM_CommonCPP_Tests/test.cpp
+++ TM_CommonCPP_Tests/test.cpp
@@ -1,7 +1,8 @@
 #include "pch.h"
 #include "TM_CommonCPP/String.h"
 
-TEST(TestCaseName, Split) {
+TEST(StringTests, Split) {
 	std::string sString = "I am a string";
 	ASSERT_TRUE(sString == "I am a string");
+	ASSERT_TRUE(sString == "I am a stringqwer");
 }
--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ import os
 class TM_CommonCPP_Conan(ConanFile):
     name = "TM_CommonCPP"
     version = "0.1"
-    license = "MIT"
+    license = ""
     url = "https://github.com/Troy1010/TM_CommonCPP"
     description = "TMinus1010's common C++ library."
     requires="Plog/0.1@Troy1010/channel"
