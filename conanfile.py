d25f40135e111444b08138a575347d962b735c19
Unknown <drmcmurderface2@mail.com>
2018-11-11 09:08:56 -0600

    source_code is now an option, not a setting

--- conanfile.py
+++ conanfile.py
@@ -11,23 +11,25 @@ class TM_CommonCPP_Conan(ConanFile):
     requires= "Plog/0.1@Troy1010/channel"
     generators="visual_studio"
     exports = "RecommendedIntegration.py"
-    settings = "os", "compiler", "build_type", "arch", "source_code" #MSBuild is nonfunctional without this line
+    settings = "os", "compiler", "build_type", "arch" #MSBuild is nonfunctional without this line
+    options = {"source_code": [False,True]}
+    default_options = {"source_code": False}
 
     def source(self):
         self.run("git clone -b beta https://github.com/Troy1010/TM_CommonCPP.git")
 
     def build(self):
-        if not self.settings.source_code:
+        if not self.options.source_code:
             vMSBuild = MSBuild(self)
             vMSBuild.build("TM_CommonCPP/TM_CommonCPP.sln")
 
     def package(self):
-        if self.settings.source_code:
-            self.copy("RecommendedIntegration.py")
-            self.copy("*.h", dst="include", src="TM_CommonCPP/TM_CommonCPP/include")
-            self.copy("*", dst="TM_CommonCPP", src="TM_CommonCPP")
-        else:
+        self.copy("*.h", dst="include", src="TM_CommonCPP/TM_CommonCPP/include")
+        if not self.options.source_code:
             self.copy("*.lib", dst="lib", src="TM_CommonCPP", keep_path=False)
+        else:
+            self.copy("RecommendedIntegration.py")
+            self.copy("*", dst="TM_CommonCPP", src="TM_CommonCPP")
 
     def package_info(self):
         self.cpp_info.includedirs = ['include']
