a30c9239490b9646e5f47f369e5e5b8fea8aae32
Unknown <drmcmurderface2@mail.com>
2018-12-03 06:35:29 -0600

    RecommendedIntegration-no longer needs old project to remove it

--- RecommendedIntegration.py
+++ RecommendedIntegration.py
@@ -12,6 +12,5 @@ def Undo(sRoot,sProj,sSln):
     with VS.DTEWrapper() as vDTEWrapper:
         with vDTEWrapper.OpenSln(sSln) as vSlnWrapper:
             with vDTEWrapper.OpenProj(sProj) as vProjWrapper:
-                with vDTEWrapper.OpenProj(os.path.join(sRoot,"TM_CommonCPP/TM_CommonCPP/TM_CommonCPP_Lib.vcxproj")) as vProjectWrapperToUnreference:
-                    vProjWrapper.RemoveProjRef(vProjectWrapperToUnreference.vProj)
-            vSlnWrapper.RemoveProj(os.path.join(sRoot,"TM_CommonCPP/TM_CommonCPP/TM_CommonCPP_Lib.vcxproj"))
+                vProjWrapper.RemoveProjRef("TM_CommonCPP_Lib")
+            vSlnWrapper.RemoveProj("TM_CommonCPP_Lib")
